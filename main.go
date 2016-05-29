bd611d1b1877e57e0daa9c5b74d01cbf98d02be1
Timothy <yexiaozhou2003@gmail.com>
2016-05-29 22:35:46 +0800

    Add routes

--- main.go
+++ main.go
@@ -1,5 +1,37 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/kataras/iris"
+	"github.com/kataras/iris/graceful"
+	"github.com/kataras/iris/middleware/logger"
+	"github.com/kataras/iris/middleware/recovery"
+)
+
 func main() {
-	//Let's start from here...
+	web := iris.New()
+
+	web.Use(logger.Default())
+	web.Use(recovery.New(os.Stderr))
+
+	web.Config().Render.Template.Directory = "./app/views"
+
+	web.Get("/hi", hi)
+
+	//Get port from environment variables, default port number is 7000
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "7000"
+	}
+
+	fmt.Println("Service is listening at:" + port)
+	graceful.Run(":"+port, time.Duration(10)*time.Second, web)
+}
+
+func hi(ctx *iris.Context) {
+	ctx.Render("index.html", map[string]interface{}{"Name": "iris"})
 }
