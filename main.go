a2fcddf7b07472390f5952185b5b9ab993903c62
Timothy <yexiaozhou2003@gmail.com>
2016-07-12 18:03:00 +0800

    Update handler name

--- main.go
+++ main.go
@@ -5,21 +5,31 @@ import (
 	"os"
 	"time"
 
+	"github.com/iris-contrib/graceful"
+	"github.com/iris-contrib/middleware/cors"
+	"github.com/iris-contrib/middleware/logger"
+	"github.com/iris-contrib/middleware/recovery"
 	"github.com/kataras/iris"
-	"github.com/kataras/iris/graceful"
-	"github.com/kataras/iris/middleware/logger"
-	"github.com/kataras/iris/middleware/recovery"
 )
 
 func main() {
 	web := iris.New()
 
-	web.Use(logger.Default())
+	web.Use(logger.New(iris.Logger))
 	web.Use(recovery.New(os.Stderr))
 
-	web.Config().Render.Template.Directory = "./app/views"
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "DELETE", "PUT", "OPTIONS"},
+		AllowedHeaders:   []string{"Content-Type", "X-ENDPOINT", "X-SERVICE", "ACCEPT", "ORIGIN", "Authorization", "X-CSRF-TOKEN", "Cookie"},
+		AllowCredentials: true,
+		Debug:            true,
+	})
 
-	web.Get("/", hi)
+	web.Use(c)
+	web.Config.Render.Template.Directory = "./app/views"
+
+	web.Get("/", indexHandler)
 	web.Static("/assets", "./app/assets", 1)
 
 	//Get port from environment variables, default port number is 7000
@@ -33,7 +43,8 @@ func main() {
 	graceful.Run(":"+port, time.Duration(10)*time.Second, web)
 }
 
-func hi(ctx *iris.Context) {
+func indexHandler(ctx *iris.Context) {
+	ctx.Response.Header.Add("Access-Control-Allow-Origin", "*")
 	if err := ctx.Render("index.html", nil); err != nil {
 		fmt.Println(err.Error())
 		panic(err)
