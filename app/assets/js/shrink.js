a26103aab880e4d21b35458a699a0294cebffd1a
Timothy <yexiaozhou2003@gmail.com>
2016-09-18 17:45:21 +0800

    Add button switch logic

--- app/assets/js/shrink.js
+++ app/assets/js/shrink.js
@@ -1,10 +1,21 @@
 var Shrink = function() {
+    var shrinkMode = true;
 
     var handleShrink = function() {
         $('#submit-btn').on('click', function(e) {
             e.preventDefault();
             console.log("clicked!")
-            //shrink_url();
+
+            if(shrinkMode) {
+              console.log("shrink mode");
+              shrinkMode = false;
+              $('#submit-btn').text("Copy");
+              //shrink_url();
+            } else {
+              console.log("copy mode");
+              shrinkMode = true;
+              $('#submit-btn').text("Shrink URL");
+            }
         });
     }
 
@@ -13,11 +24,9 @@ var Shrink = function() {
         var url = form.attr("action");
         $.post(url, form.serialize(), function(data) {
             if (data.Code != '00') {
-                $("#mobilePhone").val("");
-                bootbox.alert('<b><font color="#d9534f">' + data.Message + '</font></b>');
+                bootbox.alert('');
             } else {
-                $("#mobilePhone").val("");
-                bootbox.alert('您查询的号码:<b><font color="#337ab7">' + data.Data.PhoneNumber + '</font></b> 为 <b><font color="#337ab7">' + data.Data.Comments + '</font></b>');
+                bootbox.alert('');
             }
         }, "json");
     }
