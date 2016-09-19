bed98c8a13991f33ca99c806994b1f2238e3bf08
Timothy <yexiaozhou2003@gmail.com>
2016-09-19 16:53:52 +0800

    Add redis client

--- app/controllers/index.go
+++ app/controllers/index.go
@@ -4,9 +4,28 @@ import (
 	"fmt"
 
 	"github.com/kataras/iris"
+	"gopkg.in/redis.v4"
 )
 
 type IndexController struct {
+	redis *redis.Client
+}
+
+type Response struct {
+	Result  bool   `json:"result"`
+	Short   string `json:"short"`
+	Message string `json:"message"`
+}
+
+func init() {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	pong, err := client.Ping().Result()
+	fmt.Println(pong, err)
 }
 
 func (c *IndexController) IndexHandler(ctx *iris.Context) {
@@ -20,4 +39,16 @@ func (c *IndexController) GetShortHandler(ctx *iris.Context) {
 }
 
 func (c *IndexController) ShortURLHandler(ctx *iris.Context) {
+	url := ctx.FormValue("url")
+	resp := new(Response)
+
+	if string(url) == "" {
+		resp.Result = false
+		resp.Message = "Please input URL first..."
+	}
+
+	if err := ctx.JSON(iris.StatusOK, resp); err != nil {
+		fmt.Println(err.Error())
+		panic(err)
+	}
 }
