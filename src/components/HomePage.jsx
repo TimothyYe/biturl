c825a05bbad3a4a7cd3404f5339404d298d976e1
Timothy <yexiaozhou2003@gmail.com>
2020-05-14 12:20:27 +0800

    adjust layout

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,12 +1,19 @@
-import React from "react";
-import { Input } from "antd";
+import React, { useState } from "react";
+import { Input, Button } from "antd";
 import "../style.css";
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
 
-const { Search } = Input;
-
 function HomePage() {
+  const [urlValue, setURL] = useState("");
+
+  const onShorten = (value, e) => {
+    console.log(value);
+    setURL("*******");
+    console.log(e.target);
+    e.target.type = "dashed";
+  };
+
   return (
     <>
       <Header />
@@ -17,19 +24,25 @@ function HomePage() {
           top: "50%",
           transform: "translate(-50%, -50%)",
           color: "white",
+          width: "25vw",
+          textAlign: "center",
         }}
       >
         <h1>BitURL</h1>
         <h3>Make your links manageable.</h3>
         <br />
 
-        <div>
-          <Search
-            placeholder="Please enter URL..."
-            enterButton="SHORTEN"
-            size="large"
-            onSearch={(value) => console.log(value)}
-          />
+        <div className="input-group">
+          <div className="input-box">
+            <Input
+              readOnly={false}
+              placeholder="Please enter URL..."
+              size="default"
+            />
+          </div>
+          <Button type="primary" size="default">
+            SHORTEN
+          </Button>
         </div>
       </div>
       <Footer />
